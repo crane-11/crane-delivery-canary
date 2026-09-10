273160f54fe603206df47f1323d5f6ae1eccebd4
crane-delivery <crane-delivery@local.invalid>
2026-09-10 15:30:45 -0400

    run 8c12b147-f45b-44d1-84e8-5601a10534ce phase phase1 attempt 2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(statusMessage())
 }
 
+// statusMessage reports the canary delivery status line for CANARY-10.
 func statusMessage() string {
 	return "canary: unfiltered [canary-10]"
 }
